84bdc2696c10e93e6090007777d8623783b05733
zajer <pikus3@list.pl>
2022-07-25 17:31:46 +0200

    Added more tests of applyRoleFun function.

--- tests/core_simulation_tests.js
+++ tests/core_simulation_tests.js
@@ -19,16 +19,24 @@ describe('SimActAbstRole', () => {
 
 describe('applyRoleFun', () => {
 	before( () => {
-		this.obj1 = new SimObject(1,new Map());
-		this.obj2 = new SimObject(2,new Map());
+		//this.obj1 = new SimObject(1,new Map());
+		//this.obj2 = new SimObject(2,new Map());
 		this.impactfulRoleId = 1;
 		this.outputAttribute = "yolo";
 		this.impactfulAttributeName = "impactfulAttribute";
-		let funIfOutAttrExists = ( modifiedObj, currentValue, impactfulAttribute) => { modifiedObj.attributes.set(outputAttribute, (currentValue+impactfulAttribute)); };
-		let funIfOutAttrNotExists = ( modifiedObj, impactfulAttribute) => { modifiedObj.attributes.set(outputAttribute, impactfulAttribute); };
-		this.appliedFun = new SimRoleFun (outputAttribute, impactfulRoleId, [impactfulAttributeName], funIfOutAttrExists, funIfOutAttrNotExists );
+		let funIfOutAttrExists_singleInput = ( modifiedObj, currentValue, impactfulAttribute) => { modifiedObj.attributes.set(outputAttribute, (currentValue+impactfulAttribute)); };
+		let funIfOutAttrNotExists_singleInput = ( modifiedObj, impactfulAttribute) => { modifiedObj.attributes.set(outputAttribute, impactfulAttribute); };
+		this.appliedFun = new SimRoleFun (outputAttribute, impactfulRoleId, [impactfulAttributeName], funIfOutAttrExists_singleInput, funIfOutAttrNotExists_singleInput );
+		
+		this.impactfulAttribute2Name = "another important attribute";
+		let funIfOutAttrExists_twoInputs = ( modifiedObj, currentValue, impactfulAttribute, anotherImportantAttribute) => { modifiedObj.attributes.set(outputAttribute, (currentValue-(impactfulAttribute+anotherImportantAttribute))); };
+		let funIfOutAttrNotExists_twoInputs = ( modifiedObj, impactfulAttribute, anotherImportantAttribute) => { modifiedObj.attributes.set(outputAttribute, impactfulAttribute-anotherImportantAttribute); };
+		this.appliedFun2 = new SimRoleFun (outputAttribute, impactfulRoleId, [impactfulAttributeName,impactfulAttribute2Name], funIfOutAttrExists_twoInputs, funIfOutAttrNotExists_twoInputs);
 	});
-	
+	beforeEach( () => {
+		this.obj1 = new SimObject(1,new Map());
+		this.obj2 = new SimObject(2,new Map());
+	});
 	it('should add an attribute to an object based on a single attribute of another object', () => {
 		let abstRole1 = new SimActAbstRole(1,[this.appliedFun],[]);
 		let abstRole2 = new SimActAbstRole(2,[],[]);
@@ -57,5 +65,36 @@ describe('applyRoleFun', () => {
 		
 		expect( this.obj1.attributes.has(this.outputAttribute) ).equal(true);
 		expect( this.obj1.attributes.get(this.outputAttribute) ).equal(777);
+	});
+	it('should add an attribute to an object based on multiple attributes of another object', () => {
+		let abstRole1 = new SimActAbstRole(1,[this.appliedFun2],[]);
+		let abstRole2 = new SimActAbstRole(2,[],[]);
+		
+		let role1 = new SimActRole(abstRole1, this.obj1);
+		let role2 = new SimActRole(abstRole2, this.obj2);
+		
+		this.obj2.attributes.set(this.impactfulAttributeName, 456);
+		this.obj2.attributes.set(this.impactfulAttribute2Name, 111);
+		
+		applyRoleFun(this.obj1, this.appliedFun2, [role1,role2]);
+		
+		expect( this.obj1.attributes.has(this.outputAttribute) ).equal(true);
+		expect( this.obj1.attributes.get(this.outputAttribute) ).equal(345);
+	});
+	it('should update an attribute of an object based on multiple attributes of another object', () => {
+		let abstRole1 = new SimActAbstRole(1,[this.appliedFun2],[]);
+		let abstRole2 = new SimActAbstRole(2,[],[]);
+		
+		let role1 = new SimActRole(abstRole1, this.obj1);
+		let role2 = new SimActRole(abstRole2, this.obj2);
+		
+		this.obj1.attributes.set(this.outputAttribute, 777);
+		this.obj2.attributes.set(this.impactfulAttributeName, 456);
+		this.obj2.attributes.set(this.impactfulAttribute2Name, 111);
+		
+		applyRoleFun(this.obj1, this.appliedFun2, [role1,role2]);
+		
+		expect( this.obj1.attributes.has(this.outputAttribute) ).equal(true);
+		expect( this.obj1.attributes.get(this.outputAttribute) ).equal(210);
 	});
 })
