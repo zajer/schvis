eb88d5baacccff892e720c99b348bb6aa13208b5
zajer <pikus3@list.pl>
2022-08-04 01:16:52 +0200

    Added fucntionality of advancing simulation by a single activity. Removed setting width and height of simulation canvas to avoid blurry pictures.

--- engines.js
+++ engines.js
@@ -5,30 +5,61 @@
 //- zgłaszającą chęć resetu symulacji
 //- zgłaszającą chęć automatycznego postępu o kolejne momenty
 //- funkcję, która będą służyła jako callback oznaczający, że można dokonać postępu symulacji
+
+//potrzebne są cztery obiekty:
+//	allPlannedActivities
+//	workingAnimators
+//	simulation
+//	usedCanvas = [ {cavas,wrapper} ]
+
 const simEngine = {
-	sim : undefined,
-	reqSimProgressByActivity : () => {},
+	reqSimProgressByActivity : () => {
+		console.log("Begining the simulation progress opertation");
+		simulation.state = SimState.UnsetNotChanged;
+		let advanceSim = () => {
+			console.log("Animations are switched to transitional, modyfing the the simulation state");
+			simulation.state = SimState.UnsetChanging;
+			progressSimByAct(simulation);
+			simulation.state = SimState.Set;
+			console.log("Simulation objects modyfied going back to dynamic animations");
+			animEngine.reqSwitchToDynamicAnims();
+		};
+		animEngine.reqSwitchToTransAnims(advanceSim);
+	},
 	reqSimProgressByMoment : () => {},
 	reqSimReset : () => {},
 	reqSimPlay : () => {},
 };
 
-var workingAnimators = [];
-
-function switchToTransitionalAnimationsInAllAnimators (callMeWhenItsDone) {
-	let pendingTransitions = workingAnimators.map ( animator => {
-		return new Promise( resolve => {
-			animator.requestTransAnims( resolve );				
+const animEngine = {
+	reqSwitchToTransAnims : (callMeWhenItsDone) => {
+		let pendingTransitions = workingAnimators.map ( animator => {
+			return new Promise( resolve => {
+				animator.requestTransAnims( resolve );				
+			});
 		});
-	});
-	
-	return Promise.all( pendingTransitions ).then( () => { 
+		
+		return Promise.all( pendingTransitions ).then( () => { 
 			callMeWhenItsDone();
 		});
-}
-
-var animEngine = {
-	reqSwitchToTransAnims : switchToTransitionalAnimationsInAllAnimators
+	},
+	reqSwitchToDynamicAnims : () => {
+		workingAnimators.forEach( animator => { animator.areTransAnimsRequested = false; } );
+	},
+	animate : () => {
+		usedCanvas.forEach ( pair => {
+			let wrapperDOMRect = pair.wrapper.getBoundingClientRect();
+			pair.canvas.width = wrapperDOMRect.width-7; 
+			pair.canvas.height = wrapperDOMRect.height-7; 
+			
+			let domRect = pair.canvas.getBoundingClientRect();
+			let width = domRect.width;
+			let height = domRect.height;
+			pair.canvas.getContext('2d').clearRect(0, 0, width, height);
+		});
+		workingAnimators.forEach( animator => { animator.drawPartOfAFrame(); } );
+		window.requestAnimationFrame(animEngine.animate);
+	}
 };
 
-
+animEngine.animate();
